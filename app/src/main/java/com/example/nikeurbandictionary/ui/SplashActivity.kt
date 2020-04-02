9d24a330faaa90b0c243cdd980c8ac72c5069137
Samuel Bernal <samuelisaacbernal@hotmail.com>
2020-04-02 17:49:59 -0400

    Implement Lottie animation

---
+++ app/src/main/java/com/example/nikeurbandictionary/ui/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.nikeurbandictionary.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.HandlerCompat
+import com.example.nikeurbandictionary.R
+import com.example.nikeurbandictionary.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        showSplash()
+    }
+
+    fun startAnimation(){
+    }
+
+    private fun showSplash(){
+        Handler().postDelayed({
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+            finish()
+        }, 3000)
+    }
+}
--- app/src/main/java/com/example/nikeurbandictionary/util/Constants.kt
+++ app/src/main/java/com/example/nikeurbandictionary/util/Constants.kt
@@ -1,5 +1,5 @@
 package com.example.nikeurbandictionary.util
 
 const val SORT_FRAGMENT_TAG: String = "sort_fragment"
-const val THUMBS_UP: Boolean = true
-const val THUMBS_DOWN: Boolean = false
+const val THUMBS_UP: Boolean = false
+const val THUMBS_DOWN: Boolean = true
