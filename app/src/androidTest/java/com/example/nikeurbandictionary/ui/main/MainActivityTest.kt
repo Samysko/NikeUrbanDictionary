d467cf0bce57117daa067029e25cc956de4e430c
Samuel Bernal <samuelisaacbernal@hotmail.com>
2020-04-02 23:08:40 -0400

    Add unit testing with end to end

---
+++ app/src/androidTest/java/com/example/nikeurbandictionary/ui/main/MainActivityTest.kt
@@ -0,0 +1,60 @@
+package com.example.nikeurbandictionary.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.example.nikeurbandictionary.R
+import org.hamcrest.CoreMatchers.not
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest{
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+    
+    @Test
+    fun shouldHaveItemsInside(){
+        // Given
+        val myEditText = onView(ViewMatchers
+            .withId(R.id.etWordSearch))
+        val myRecyclerView = onView(ViewMatchers
+            .withId(R.id.rvWordList))
+
+        // When
+        myEditText.perform(ViewActions
+            .replaceText("house"))
+
+        // Assert
+        myRecyclerView.check { view, noViewFoundException ->
+            noViewFoundException?.apply {
+                throw this
+            }
+            assertTrue(view is RecyclerView &&
+                    view.adapter != null && view.adapter?.itemCount?:-1 > 0
+            )
+
+        }
+    }
+
+    @Test
+    fun shouldNotBeEmpty(){
+        // Given
+        val mainEditText = onView(ViewMatchers.withId(R.id.etWordSearch))
+
+        // When
+        mainEditText.perform(ViewActions
+            .replaceText("hello"))
+
+        // Assert
+        mainEditText.check(matches(not("")))
+    }
+}
