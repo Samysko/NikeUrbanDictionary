6bd6237dab60b4e66c8349ed7791f7d0bb5a1ec4
Samuel Bernal <samuelisaacbernal@hotmail.com>
2020-04-03 13:28:52 -0400

    Fix unit test case

--- app/src/test/java/com/example/nikeurbandictionary/NetworkTest.kt
+++ app/src/test/java/com/example/nikeurbandictionary/NetworkTest.kt
@@ -1,9 +1,13 @@
 package com.example.nikeurbandictionary
 
+import android.content.Context
+import androidx.test.platform.app.InstrumentationRegistry
 import com.example.nikeurbandictionary.service.UrbanApi
 import com.example.nikeurbandictionary.util.MainCoroutineRule
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -11,13 +15,20 @@ class NetworkTest {
     @get:Rule
     val mainCoroutineRule = MainCoroutineRule()
 
+    lateinit var instrumentationContext: Context
+
+    @Before
+    fun setup() {
+        instrumentationContext = InstrumentationRegistry.getInstrumentation().context
+    }
+
     @Test
     fun shouldFetchDataFromRestServer() = runBlocking {
             // Given
             val term = "hey"
 
             // When
-            val responseList = UrbanApi.create().getWordDefinitions(term).list
+            val responseList = UrbanApi.create(instrumentationContext).getWordDefinitions(term).list
 
             // Then
             Assert.assertTrue(responseList.isNotEmpty())
