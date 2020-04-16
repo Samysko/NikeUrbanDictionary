8858786874a119a598f14df5ade679d85685965b
Samuel Bernal <samuelisaacbernal@hotmail.com>
2020-04-16 14:40:28 -0400

    Update SearchedWord

--- app/src/main/java/com/example/nikeurbandictionary/extensions/list/ListExt.kt
+++ app/src/main/java/com/example/nikeurbandictionary/extensions/list/ListExt.kt
@@ -2,6 +2,12 @@ package com.example.nikeurbandictionary.extensions.list
 
 import com.example.nikeurbandictionary.model.SearchedWord
 
+/**
+ * Used to sort a Searched Word List by likes, dislikes otherwise it will return the default list's order.
+ *
+ * @param sortBy Indicates weather the list will be sorted by number of thumbs up or thumbs down
+ * @param sortingEnabled Indicates if the sort is enabled
+ */
 fun List<SearchedWord>.sortByLikes(sortBy: Boolean, sortingEnabled: Boolean) = when {
     sortingEnabled && sortBy -> this.sortedByDescending { it.thumbs_up }
     sortingEnabled -> this.sortedByDescending { it.thumbs_down }
